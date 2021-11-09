4f8e3134dac775a4abd51824f8402de130d81a2b
thanhhoax701 <tth1610.vl@gmail.com>
2021-11-09 18:50:40 +0700

    update

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         dots: true,
         prevArrow: "<button type='button' class='slick-prev slick-arrow'><i class='fas fa-arrow-left'></i></button>",
         nextArrow: "<button type='button' class='slick-next slick-arrow'><i class='fas fa-arrow-right' aria-hidden='true'></i></button>",
-        autoplay: true,
+        // autoplay: true,
         autoplaySpeed: 2000,
         responsive: [{
                 breakpoint: 1024,
@@ -37,3 +37,31 @@ $(document).ready(function() {
         ]
     });
 });
+
+
+// Password
+var pass = document.getElementById("password");
+var msg = document.getElementById("message");
+var str = document.getElementById("strenght");
+
+pass.addEventListener('input', () => {
+    if (pass.value.length > 0) {
+        msg.style.display = "block";
+    } else {
+        msg.style.display = "none";
+    }
+    if (pass.value.length < 4) {
+        str.innerHTML = "weak";
+        pass.style.borderColor = "#ff5925";
+        msg.style.color = "#ff5925";
+    } else if (pass.value.length >= 4 && pass.value.length < 8) {
+        str.innerHTML = "medium";
+        pass.style.borderColor = "#fbd233";
+        msg.style.color = "#fbd233";
+    } else if (pass.value.length >= 8) {
+        str.innerHTML = "strong";
+        pass.style.borderColor = "#26d730";
+        msg.style.color = "#26d730";
+    }
+
+})
